db139c1d23e1692e3a7ab7e45072ed3e480223c4
Daniel Cruz <d.afc98@gmail.com>
2022-12-27 21:13:41 -0300

    final do curso

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,47 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_webapi_first_course/services/journal_service.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'models/journal.dart';
+import 'screens/add_journal_screen/add_journal_screen.dart';
 import 'screens/home_screen/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
-
-  JournalService service = JournalService();
-  service.register("Olá Mundo!");
-  service.get();
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Simple Journal',
       debugShowCheckedModeBanner: false,
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.light,
       theme: ThemeData(
         primarySwatch: Colors.grey,
         appBarTheme: const AppBarTheme(
-            backgroundColor: Colors.black,
-            elevation: 0,
-            titleTextStyle: TextStyle(color: Colors.white)
+          elevation: 0,
+          backgroundColor: Colors.black,
+          titleTextStyle: TextStyle(color: Colors.white),
+          actionsIconTheme: IconThemeData(color: Colors.white),
+          iconTheme: IconThemeData(color: Colors.white),
         ),
-        textTheme: GoogleFonts.bitterTextTheme()
+        textTheme: GoogleFonts.bitterTextTheme(),
       ),
-      darkTheme: ThemeData.dark(),
-      themeMode: ThemeMode.light,
       initialRoute: "home",
       routes: {
         "home": (context) => const HomeScreen(),
+      },
+      onGenerateRoute: (routeSettings) {
+        if (routeSettings.name == "add-journal") {
+          final journal = routeSettings.arguments as Journal;
+          return MaterialPageRoute(
+            builder: (context) {
+              return AddJournalScreen(journal: journal);
+            },
+          );
+        }
+        return null;
       },
     );
   }
